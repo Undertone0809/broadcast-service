eaa225c9debdfcdbbee66da813e7c46e59e967c7
Zeeland <zeeland@foxmail.com>
2023-01-10 02:12:35 +0800

    feat: add decorator, optimize syntactic expression

--- broadcast_service/_core.py
+++ broadcast_service/_core.py
@@ -13,8 +13,11 @@
 # limitations under the License.
 
 from concurrent.futures import ThreadPoolExecutor
+from functools import wraps
+from typing import Optional, List, Callable
 
-__all__ = ['broadcast_service']
+__all__ = ['broadcast_service', 'BroadcastService']
+
 
 class BroadcastService:
     """
@@ -23,7 +26,6 @@ class BroadcastService:
     callback function if some classes subscribe the topic.
 
     example:
-    app.py
     ---------------------------------------------------------------------------------
     from broadcast_service import broadcast_service
 
@@ -34,67 +36,111 @@ class BroadcastService:
         info = 'This is very important msg'
 
         # listen topic
-        broadcast_service.listen('Test', handle_msg)
+        broadcast_service.subscribe('Test', handle_msg)
 
         # publish broadcast
-        broadcast_service.broadcast('Test', info)
+        broadcast_service.publish('Test', info)
     ---------------------------------------------------------------------------------
-
     """
 
     def __init__(self):
         """
-        subscribe_info example:
+        pubsub_channels is the dict to store publish/subscribe data.
+        pubsub_channels example:
 
-        subscribe_info = {
-            'my_topic': [{
-                'callback_function': function1,
-                'params': {
-                    'name':'jack',
-                    'age':20
-                }
-            },{
-                'callback_function': function2,
-                'params': 666
-            }]
+        pubsub_channels = {
+            'my_topic': [callback_function1: Callable,callback_function2: Callable]
+            '__all__': [callback_function3: Callable]
         }
         """
-        self.topic_list = []
-        self.subscribe_info = {}
-        self.enable_async = True
+        self.pubsub_channels: dict = {
+            '__all__': []
+        }
+        self.enable_async: bool = True
         self.thread_pool = ThreadPoolExecutor(max_workers=5)
 
-    def listen(self, topic_name, callback):
+        # function renaming
+        self.subscribe = self.listen
+        self.publish = self.broadcast
+        self.unsubscribe = self.stop_listen
+
+        self.on = self.listen
+        self.emit = self.broadcast
+        self.off = self.stop_listen
+
+    def listen(self, topic_name: str, callback: Callable):
         """ listen topic """
-        if topic_name not in self.subscribe_info.keys():
-            self.subscribe_info[topic_name] = []
+        if topic_name not in self.pubsub_channels.keys():
+            self.pubsub_channels[topic_name] = []
 
-        if callback not in self.subscribe_info[topic_name]:
-            options = {
-                'callback_function': callback,
-            }
-            self.subscribe_info[topic_name].append(options)
+        if callback not in self.pubsub_channels[topic_name]:
+            # options = {
+            #     'callback_function': callback,
+            # }
+            self.pubsub_channels[topic_name].append(callback)
 
-    def broadcast(self, topic_name, *args, **kwargs):
-        """ Launch broadcast on the specifide topic """
-        if topic_name not in self.topic_list:
-            self.topic_list.append(topic_name)
+    def broadcast(self, topic_name: str, *args, **kwargs):
+        """
+        Launch broadcast on the specify topic
+        """
+        if topic_name not in self.pubsub_channels.keys():
+            self.pubsub_channels[topic_name] = []
 
-        if topic_name not in self.subscribe_info.keys():
-            return
-
-        for item in self.subscribe_info[topic_name]:
+        for item in self.pubsub_channels[topic_name]:
             if self.enable_async:
-                self.thread_pool.submit(item['callback_function'], *args, **kwargs)
+                self.thread_pool.submit(
+                    item, *args, **kwargs)
             else:
-                item['callback_function'](*args, **kwargs)
+                item(*args, **kwargs)
 
-    def stop_listen(self, topic_name, callback):
-        if topic_name not in self.subscribe_info.keys():
+        for item in self.pubsub_channels['__all__']:
+            if self.enable_async:
+                self.thread_pool.submit(
+                    item, *args, **kwargs)
+            else:
+                item(*args, **kwargs)
+
+    def stop_listen(self, topic_name: str, callback: Callable):
+        if topic_name not in self.pubsub_channels.keys():
             raise RuntimeError("you didn't listen the topic:", topic_name)
-        if callback not in self.subscribe_info[topic_name]:
+        if callback not in self.pubsub_channels[topic_name]:
             pass
         else:
-            self.subscribe_info[topic_name].remove(callback)
+            self.pubsub_channels[topic_name].remove(callback)
+
+    def on_listen(self, topics: Optional[List[str]] = None) -> Callable:
+        """
+        Decorator to listen specify topic. If topics is none, then listen all topics has exits.
+        :param topics: topic list, you can input topic like: ["topic1", "topic2"].
+
+        Usage::
+            @broadcast_service.on_listen(['topic1'])
+            def handle_all_msg():
+                # your code
+
+            @broadcast_service.on_listen(['topic1','topic2'])
+            def handle_all_msg():
+                # your code
+
+            @broadcast_service.on_listen()
+            def handle_all_msg(*args, **kwargs):
+                # your code
+
+        Attention: Your params should keep '*args, **kwargs'. If you publish a topic take arguments,
+        the callback function you handle should take arguments, otherwise it will not be called back.
+        """
+        def decorator(fn: Callable) -> Callable:
+            if topics is not None:
+                for topic in topics:
+                    self.listen(topic, fn)
+            else:
+                self.listen('__all__', fn)
+
+            def inner(*args, **kwargs) -> Callable:
+                ret = fn(*args, **kwargs)
+                return ret
+            return inner
+        return decorator
+
 
 broadcast_service = BroadcastService()
